ed5731fe908e6290b83b34d1c20356dcd6d7431e
OmegaWulf <nikcxz@gmail.com>
2019-01-23 16:09:12 -0500

    Storyboard

---
+++ objc-documents/objc-documents/NKTDetailVC.h
@@ -0,0 +1,22 @@
+//
+//  NKTDetailVC.h
+//  objc-documents
+//
+//  Created by Nikita Thomas on 1/23/19.
+//  Copyright © 2019 Nikita Thomas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NKTDetailVC : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *countLabel;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ objc-documents/objc-documents/NKTDocumentsTVC.h
@@ -0,0 +1,17 @@
+//
+//  NKTDocumentsTVC.h
+//  objc-documents
+//
+//  Created by Nikita Thomas on 1/23/19.
+//  Copyright © 2019 Nikita Thomas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NKTDocumentsTVC : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
